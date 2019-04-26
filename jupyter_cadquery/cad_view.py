cdd9f8943ffcb02d6db8f729100981fa6a9f2a95
Bernhard <bernhard.walter@me.com>
2019-04-26 16:21:07 +0200

    addedd license info

--- jupyter_cadquery/cad_view.py
+++ jupyter_cadquery/cad_view.py
@@ -1,3 +1,17 @@
+# Copyright 2019 Bernhard Walter
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import math
 
 import warnings
@@ -240,7 +254,7 @@ class CadqueryView(object):
     def _norm(self, vec):
         n = np.linalg.norm(vec)
         return [v/n for v in vec]
-    
+
     def _minus(self, vec):
         return [-v for v in vec]
 
--- jupyter_cadquery/tree_view.py
+++ jupyter_cadquery/tree_view.py
@@ -1,3 +1,17 @@
+# Copyright 2019 Bernhard Walter
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import base64
 
 from traitlets import Unicode, List, Dict
@@ -16,9 +30,9 @@ def state_diff(old_states, new_states):
         if old_states[key] != new_states[key]:
             for i in range(len(old_states[key])):
                 if old_states[key][i] != new_states[key][i]:
-                    result.append({key: {"icon": i, "new":new_states[key][i]}})
+                    result.append({key: {"icon": i, "new": new_states[key][i]}})
     return result
-    
+
 @register
 class TreeView(Button):
     """An example widget."""
