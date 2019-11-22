656c393478926187857b4acaa2b44c62ceb1b0d8
Bernhard <b_walter@arcor.de>
2019-11-22 17:38:00 +0100

    Latest release: 0.9.2-rc5

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ class NPM(Command):
 
 setup_args = {
     'name': 'jupyter_cadquery',
-    'version': "0.9.2-rc4",
+    'version': "0.9.2-rc5",
     'description': 'An extension to render cadquery objects in JupyterLab via pythreejs',
     'long_description': LONG_DESCRIPTION,
     'include_package_data': True,
