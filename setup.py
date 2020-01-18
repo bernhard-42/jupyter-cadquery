c936a6e8df5e95dc999ef752ed00668afdb6c8ea
Bernhard <bwalter42@gmail.com>
2020-01-18 15:47:59 +0100

    turn off prerelease tasks

--- setup.py
+++ setup.py
@@ -141,12 +141,12 @@ setup_args = {
     ],
     'packages': find_packages(),
     'zip_safe': False,
-    'cmdclass': {
-        'build_py': js_prerelease(build_py),
-        'egg_info': js_prerelease(egg_info),
-        'sdist': js_prerelease(sdist, strict=True),
-        'jsdeps': NPM,
-    },
+    # 'cmdclass': {
+    #     'build_py': js_prerelease(build_py),
+    #     'egg_info': js_prerelease(egg_info),
+    #     'sdist': js_prerelease(sdist, strict=True),
+    #     'jsdeps': NPM,
+    # },
 
     'author': 'Bernhard Walter',
     'author_email': 'b_walter@arcor.de',
