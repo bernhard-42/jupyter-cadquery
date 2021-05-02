6935bc848d4e836f8b1f99ab0459e48f3dfd3f53
Bernhard <bwalter42@gmail.com>
2021-05-02 17:57:50 +0200

    fixed show command

--- examples/ide/1-disk_arm.py
+++ examples/ide/1-disk_arm.py
@@ -70,7 +70,7 @@ disk_arm.mate("base@faces@>Z", name="arm_pivot")
 disk_arm.mate("disk@faces@>Z[-2]", name="disk", origin=True)
 disk_arm.mate("arm?mate", name="arm", origin=True)
 
-# show_object(disk_arm, reset=False)
+# show(disk_arm, reset=False)
 
 disk_arm.assemble("arm", "arm_pivot")
 disk_arm.assemble("disk", "disk_pivot")
