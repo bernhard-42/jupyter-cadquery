09203611cb47048fda5b21ef7fc85a253cd275f9
Bernhard <b_walter@arcor.de>
2021-11-17 08:50:45 +0100

    removed unused packages from dependencies

--- setup.py
+++ setup.py
@@ -22,15 +22,10 @@ setup_args = {
     "include_package_data": True,
     "python_requires": ">=3.6",
     "install_requires": [
-        "ipywidgets~=7.6",
         "webcolors~=1.11",
         "notebook~=6.3",
-        "sidecar~=0.5",
-        "jupyter-cadquery-widgets~=2.0",
-        "pythreejs~=2.3",
         "voila~=0.2",
         "cadquery_massembly~=0.9",
-        "orbitcontrol-patch~=0.1.0",
         "pyquaternion~=0.9.9",
     ],
     "extras_require": {
